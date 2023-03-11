361303cbc0bf2efb14a9f7c73217fc9e9779bfe7
Admin <anonymousinko@mail.ru>
2023-03-11 18:13:53 +0300

    created function generate_qr

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import qrcode
+def generate_qr(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="white", back_color="black")
+    img.save("qr_code.png")
 
-
+generate_qr("https://www.youtube.com/watch?v=pdy3nh1tn6I")
